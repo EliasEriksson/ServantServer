f7fef9ed2e4b738418d3771e6c2c0c7617423bbc
eliaseriksson <eliaseriksson95@gmail.com>
2020-02-20 16:08:00 +0100

    initial commit

--- Server/main.py
+++ Server/main.py
@@ -1,7 +1,11 @@
 from Server.watcher import Watcher
 from Server import settings
+from pathlib import Path
+import json
 
 
 if __name__ == '__main__':
     watcher = Watcher(settings["activity_name"])
-    watcher.run("NTU1ODQ1MzI4MTUxMTE3ODQ1.XkFFbw.spXyVpNdhH9k2Xn9H4L6cvJZykE")
+    with Path("discord_bot_token.json").open() as file:
+        bot_token = json.load(file)
+    watcher.run(bot_token)
